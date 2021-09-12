b778ec4c7e72967d077f395ad326e4679692a2c2
Kevin Chen <kc3585@nyu.edu>
2021-09-12 14:08:28 +0000

    check for metamask ethereum account and connect account

--- src/App.js
+++ src/App.js
@@ -4,6 +4,49 @@ import './App.css';
 
 export default function App() {
 
+  const [currAccount, setCurrentAccount] = React.useState("");
+
+  const checkIfWalletIsConnected = () => { 
+    const { ethereum } = window; 
+    if (!ethereum) {
+      console.log("Make sure you have metamask!")
+      return
+    } 
+    else {
+      console.log("We have the ethereum object", ethereum)
+    }
+
+    ethereum.request({ method: 'eth_accounts' })
+    .then(accounts => {
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account)
+        setCurrentAccount(account)
+      }
+      else {
+        console.log("No authorized account found")
+      }
+    })
+  }
+
+  const connectWallet = () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("Get metamask!")
+    }
+
+    ethereum.request({ method: 'eth_requestAccounts' })
+    .then(accounts => {
+      console.log("Connected", accounts[0])
+      setCurrentAccount(accounts[0])
+    })
+    .catch(err => console.log(err));
+  }
+
+  React.useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   const wave = () => {
     
   }
@@ -23,6 +66,10 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        { currAccount ? null : (
+          <button className="waveButton" onClick={connectWallet}> Connect Wallet </button>
+        )}
       </div>
     </div>
   );
